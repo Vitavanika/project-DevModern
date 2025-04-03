fa515294092b1dec44ae9c70d1332cde19362554
hanna-uler <hannauler@gmail.com>
2025-04-03 16:29:11 -0400

    add aditional div for swiper, initial swiper in js

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,5 +1,17 @@
 import Swiper from 'swiper';
 // import Swiper styles
 import 'swiper/css';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css/navigation';
 
-const swiper = new Swiper(...);
+const swiperOptions = {
+  direction: 'horizontal',
+  navigation: {
+    nextEl: '.swiper-btn-next',
+    prevEl: '.swiper-btn-prev',
+  },
+};
+
+const swiper = new Swiper('.swiper', swiperOptions, {
+  module: Navigation,
+});
