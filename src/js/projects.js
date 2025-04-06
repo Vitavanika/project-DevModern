0c9157e66dc074a24d7354c4b27b3732cfaa1c34
hanna-uler <hannauler@gmail.com>
2025-04-05 23:25:01 -0400

    add conditions for btns enable/disable

--- src/js/projects.js
+++ src/js/projects.js
@@ -3,6 +3,10 @@ import 'swiper/css';
 
 const nextBtnEl = document.querySelector(".swiper-btn-next");
 const prevBtnEl = document.querySelector(".swiper-btn-prev");
+const slideEl = document.querySelector(".swiper-slide")
+const lastSlideEl = document.querySelector(".swiper-slide:last-child");
+prevBtnEl.disabled = true;
+nextBtnEl.disabled = false;
 
 const swiperOptions = {
     slidesPerView: 1,
@@ -13,13 +17,27 @@ const swiperOptions = {
 };
 
 const swiper = new Swiper('.swiper', swiperOptions);
-
 const slideFoward = (event) => {
+    event.preventDefault(); 
     swiper.slideNext()
+    prevBtnEl.disabled = false;
+    if (swiper.isEnd) {
+        nextBtnEl.disabled = true;
+    }
 };
+
 const slideBack = (event) => {
+    event.preventDefault();
     swiper.slidePrev();
+    nextBtnEl.disabled = false;
+
+    if (swiper.isBeginning) {
+        prevBtnEl.disabled = true;
+    }
 };
 
 nextBtnEl.addEventListener("click", slideFoward);
 prevBtnEl.addEventListener("click", slideBack);
+nextBtnEl.addEventListener("touchstart", slideFoward);
+prevBtnEl.addEventListener("touchstart", slideBack);
+
