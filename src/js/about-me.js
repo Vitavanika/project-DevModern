b63f1c9263f5b6a1ed866a99adbc04346a5de4f9
kostyaslob <kostyaslob@gmail.com>
2025-04-07 15:41:50 +0300

    update accordion

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -6,28 +6,28 @@ import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 
 
-new Accordion(".accordion-container", {
-    duration: 400,
-    openOnInit: [0],
+const aboutAccordion = new Accordion(".about-ac-container", {
+  duration: 500,
+  openOnInit: [0],
 });
 
 
-const faqs = document.querySelectorAll(".faq");
+// const faqs = document.querySelectorAll(".faq");
 
-if (faqs.length > 0) {
-    faqs[0].classList.add("active");
-}
+// if (faqs.length > 0) {
+//     faqs[0].classList.add("active");
+// }
 
-faqs.forEach(faq => {
-    faq.addEventListener("click", () => {
-        faqs.forEach(item => {
-            if (item !== faq) {
-                item.classList.remove("active");
-            }
-        });
-        faq.classList.toggle("active");
-    });
-});
+// faqs.forEach(faq => {
+//     faq.addEventListener("click", () => {
+//         faqs.forEach(item => {
+//             if (item !== faq) {
+//                 item.classList.remove("active");
+//             }
+//         });
+//         faq.classList.toggle("active");
+//     });
+// });
 
 const aboutSwiper = new Swiper('.about-swiper', {
   spaceBetween: 0,  
