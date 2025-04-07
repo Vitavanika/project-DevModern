0d4458964e6241f7bbfc6a9e7ca2a9148b2f1959
Hanna Uler <hannauler@gmail.com>
2025-04-07 08:19:53 -0400

    Projects section (#14)

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,17 +1,48 @@
-// import Swiper from 'swiper';
-// // import Swiper styles
-// import 'swiper/css';
-// import { Navigation } from 'swiper/modules';
-// import 'swiper/css/navigation';
+import Swiper from 'swiper';
+import 'swiper/css';
+import { Keyboard } from 'swiper/modules';
+import 'swiper/css/keyboard';
 
-// const swiperOptions = {
-//   direction: 'horizontal',
-//   navigation: {
-//     nextEl: '.swiper-btn-next',
-//     prevEl: '.swiper-btn-prev',
-//   },
-// };
+const nextPrjBtnEl = document.querySelector(".prjts-next-btn-js");
+const prevPRJBtnEl = document.querySelector(".prjts-back-btn-js");
 
-// const swiper = new Swiper('.swiper', swiperOptions, {
-//   module: Navigation,
-// });
+prevPRJBtnEl.disabled = true;
+nextPrjBtnEl.disabled = false;
+
+const swiperOptions = {
+    modules: [Keyboard],
+    slidesPerView: 1,
+    speed: 1000,
+    spaceBetween: 20,
+    allowTouchMove: true,
+    grabCursor: true,
+    keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+        pageUpDown: false,
+    },
+};
+
+const projectsSwiper = new Swiper('.projects-swiper', swiperOptions);
+
+const updatePrjSwiperBtns	 = (event) => {
+   if (projectsSwiper.isBeginning) {
+    prevPRJBtnEl.disabled = true;
+    nextPrjBtnEl.disabled = false; 
+} else if (projectsSwiper.isEnd) {
+    prevPRJBtnEl.disabled = false;
+    nextPrjBtnEl.disabled = true;
+} else{
+    prevPRJBtnEl.disabled = false;
+    nextPrjBtnEl.disabled = false;}
+}
+projectsSwiper.on("slideChange", updatePrjSwiperBtns	);
+
+
+nextPrjBtnEl.addEventListener("click", (event) => {
+    event.preventDefault(); 
+    projectsSwiper.slideNext();
+});
+prevPRJBtnEl.addEventListener("click", (event) => { event.preventDefault();
+    projectsSwiper.slidePrev();});
+
